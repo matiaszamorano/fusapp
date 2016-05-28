742ebd4d4acb5af989acd67f27c8b8344716317a
uva <uva@uva-PC>
2016-05-28 15:55:01 -0300

    Listas del usuario en perfil

--- www/js/app/ui/perfil.js
+++ www/js/app/ui/perfil.js
@@ -2,10 +2,14 @@ comunidadfusa.ui.perfil = (function () {
 
     function init() {
         var listas = comunidadfusa.service.listas.getListasUsuario();
+        var usuario = comunidadfusa.service.usuario.get();
         if (listas.length > 0) {
             $("#listas").empty();
             $("#listas").append($("#perfil-lista-usuario-tmpl").tmpl(listas));
         }
+        $(".fusa-js-avatar-usuario").attr("src", usuario.avatar);
+        $(".fusa-js-perfil-apodo").text(usuario.apodo);
+        $(".fusa-js-perfil-ciudad").text(usuario.ciudad);
     }
 
 
