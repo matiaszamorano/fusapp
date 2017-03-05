75ebda3272132b77cd6a5953d17101686ea34a0a
Uva <Uva@Uva-PC>
2017-03-05 00:20:03 -0300

    Descargas pararelas

--- www/js/app/ui/disco.js
+++ www/js/app/ui/disco.js
@@ -5,7 +5,7 @@ comunidadfusa.ui.disco = (function () {
     var fileURL = "";
     var imagePath = "";
     var fileTransfer;
-    var idAudio;
+
 
     function init() {
         var idDisco = comunidadfusa.getUrlParameter("id");
@@ -37,60 +37,56 @@ comunidadfusa.ui.disco = (function () {
             if (!$this.is('a')) {
                 $this = $this.closest('a');
             }
-            idAudio = $this.data("id");
+            var idAudio = $this.data("id");
             $this.find("i").removeClass("icon-arrow-down");
             $this.find("i").addClass("icon-clock");
             comunidadfusa.service.audios.getAudio(idAudio, function (data) {
-                descargarCancion(data.archivo);
+                descargarCancion(data.archivo, idAudio);
             });
         });
     }
 
-    function descargarCancion(nombreArchivo) {
+    function descargarCancion(nombreArchivo, idAudio) {
         filename = nombreArchivo;
         fileURL = comunidadfusa.MP3_URI + nombreArchivo;
         fileTransfer = new FileTransfer();
         try {
-            window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, onFileSystemSuccess, onError);
+            var idAudio1 = idAudio;
+            window.resolveLocalFileSystemURL(cordova.file.externalDataDirectory, function (fileSystem) {
+                entry = fileSystem;
+                entry.getDirectory("fusa", {create: true, exclusive: false}, function (dir) {
+                    var idAudio2 = idAudio1;
+                    dir.getDirectory("audios", {create: true, exclusive: false}, function (dir) {
+                        var idAudio3 = idAudio2;
+                        cdr = dir;
+                        dir.getFile(filename, {create: true, exclusive: false}, function () {
+                            var uri = encodeURI(fileURL);
+                            var idAudio4 = idAudio3;
+                            fileTransfer.download(uri, cdr.nativeURL + filename,
+                                    function (entry) {
+                                        comunidadfusa.service.audios.audioDescargado(idAudio4, cdr.nativeURL + filename);
+                                        var $cancion = $("a[data-id='" + idAudio4 + "']");
+                                        var $icono = $cancion.find("i.icon-clock");
+                                        $icono.removeClass("icon-clock");
+                                        $icono.addClass("icon-check");
+                                        $icono.addClass("text-success");
+                                    },
+                                    function (error) {
+                                        $this.find("i").removeClass("icon-clock");
+                                        $this.find("i").addClass("icon-arrow-down");
+                                        alert("download error source " + error.source);
+                                        alert("download error target " + error.target);
+                                        alert("upload error code" + error.code);
+                                        alert("error");
+                                    },
+                                    true);
+                        }, errorHandler);
+                    }, onGetDirectoryFail);
+                }, onGetDirectoryFail);
+            }, onError);
         } catch (err) {
             alert("ER - " + err.message);
         }
-    }
-
-    function onFileSystemSuccess(fileSystem) {
-        entry = fileSystem;
-        entry.getDirectory("fusa", {create: true, exclusive: false}, onGetDirectorySuccess, onGetDirectoryFail);
-    }
-
-    function onGetDirectorySuccess(dir) {
-        dir.getDirectory("audios", {create: true, exclusive: false}, onGetDirectorySuccess1, onGetDirectoryFail);
-    }
-
-    function onGetDirectorySuccess1(dir) {
-        cdr = dir;
-        dir.getFile(filename, {create: true, exclusive: false}, gotFileEntry, errorHandler);
-    }
-
-    function gotFileEntry(fileEntry) {
-        var uri = encodeURI(fileURL);
-        fileTransfer.download(uri, cdr.nativeURL + filename,
-                function (entry) {
-                    comunidadfusa.service.audios.audioDescargado(idAudio, cdr.nativeURL + filename);
-                    var $cancion = $("a[data-id='" + idAudio + "']");
-                    var $icono = $cancion.find("i.icon-clock");
-                    $icono.removeClass("icon-clock");
-                    $icono.addClass("icon-check");
-                    $icono.addClass("text-success");
-                },
-                function (error) {
-                    $this.find("i").removeClass("icon-clock");
-                    $this.find("i").addClass("icon-arrow-down");
-                    alert("download error source " + error.source);
-                    alert("download error target " + error.target);
-                    alert("upload error code" + error.code);
-                    alert("error");
-                },
-                true);
     }
 
     function  errorHandler(e) {
