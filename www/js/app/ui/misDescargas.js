ac72895efa7bb43e51f1f3d6dacff1854aeb3848
Uva <Uva@Uva-PC>
2017-03-12 22:06:07 -0300

    Ahora si, guardo en la tarjeta

--- www/js/app/ui/misDescargas.js
+++ www/js/app/ui/misDescargas.js
@@ -1,12 +1,16 @@
 comunidadfusa.ui.misDescargas = (function () {
-    
+
     var storage = window.localStorage;
 
     function init() {
         var bandasStorage = JSON.parse(storage.getItem("bandasGuardadas"));
         $(".fusa-js-bandas-descargadas").empty();
-        $(".fusa-js-bandas-descargadas").append($("#banda-descargada-tmpl").tmpl(bandasStorage));
-        comunidadfusa.util.html5HistoryAPI.setupHistoryClicks();
+        if (bandasStorage) {
+            $(".fusa-js-bandas-descargadas").append($("#banda-descargada-tmpl").tmpl(bandasStorage));
+            comunidadfusa.util.html5HistoryAPI.setupHistoryClicks();
+        } else {
+            $(".fusa-js-bandas-descargadas").html("<div class='m-sm'>Sin canciones descargadas</div>");
+        }
     }
 
     return {
