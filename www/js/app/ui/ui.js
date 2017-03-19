49176849543fdc166fe73a68a84df76baf3087fb
Uva <Uva@Uva-PC>
2017-03-19 18:42:29 -0300

    Avanzo con el buscador

--- www/js/app/ui/ui.js
+++ www/js/app/ui/ui.js
@@ -1,5 +1,7 @@
 comunidadfusa.ui = (function () {
 
+    var storage = window.localStorage;
+
     function init() {
         $(document).on("click", ".fusa-js-buscar ", function (data) {
             $(".fusa-js-buscador-inactivo").hide();
@@ -7,15 +9,20 @@ comunidadfusa.ui = (function () {
             $(".fusa-js-buscador-activo").attr("style", "display: inline-table;");
             return false;
         });
-        
+
         $(document).on("click", ".fusa-js-salir-busqueda ", function (data) {
             $(".fusa-js-buscador-inactivo").show();
             $(".fusa-js-buscador-activo").hide();
-//            $(".fusa-js-buscador-activo").attr("style", "display: inline-table;");
             return false;
         });
-        
-       
+
+        $(document).on("click", ".fusa-js-ejecutar-busqueda ", function (data) {
+            var busqueda = $(".fusa-js-busqueda").val();
+            storage.setItem("busqueda", busqueda);
+            comunidadfusa.util.html5HistoryAPI.cargarPagina("busqueda.html");
+            return false;
+        });
+
     }
 
     return {
