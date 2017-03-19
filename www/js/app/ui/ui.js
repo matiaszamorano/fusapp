e89031e789641f0a2d8278226e0479e7c1a4708f
Uva <Uva@Uva-PC>
2017-03-19 18:24:30 -0300

    Ubicando el buscador

--- www/js/app/ui/ui.js
+++ www/js/app/ui/ui.js
@@ -7,6 +7,15 @@ comunidadfusa.ui = (function () {
             $(".fusa-js-buscador-activo").attr("style", "display: inline-table;");
             return false;
         });
+        
+        $(document).on("click", ".fusa-js-salir-busqueda ", function (data) {
+            $(".fusa-js-buscador-inactivo").show();
+            $(".fusa-js-buscador-activo").hide();
+//            $(".fusa-js-buscador-activo").attr("style", "display: inline-table;");
+            return false;
+        });
+        
+       
     }
 
     return {
