71fd64efd7d215ae35f68b53e5240ceccbca367d
jonyb <jonyb@DESKTOP-JNE0F39>
2017-03-20 07:33:10 -0300

    llamada al servicepara obtener listas

--- www/js/app/service/listas/listas.js
+++ www/js/app/service/listas/listas.js
@@ -10,8 +10,31 @@ comunidadfusa.service.listas = (function () {
         comunidadfusa.service.getFromStorage(url, callback);
     }
 
+    function getMixFusero(idBanda, callback) {
+        var url = comunidadfusa.service.baseURI + "/audio/mix/" + idBanda;
+        comunidadfusa.service.getFromStorage(url, callback);
+    }
+
+    function getListasHome(callback) {
+        var bandasMasEscuchadasDeLaSemana;
+        comunidadfusa.service.bandas.getMasEscuchadasDeLaSemana(data, function (data) {
+            bandasMasEscuchadasDeLaSemana = data;
+        });
+
+        var listasDeHome = [];
+        $.each(bandasMasEscuchadasDeLaSemana, function (el, i) {
+
+//            poner nombre, imagen a la lista
+            getMixFusero(el.id, function (data) {
+                listasDeHome.push(data);
+            });
+        });
+    }
+
     return {
         getListasUsuario: getListasUsuario,
-        getAudiosListasReproduccion: getAudiosListasReproduccion
+        getAudiosListasReproduccion: getAudiosListasReproduccion,
+        getMixFusero: getMixFusero,
+        getListasHome: getListasHome
     };
 })();
