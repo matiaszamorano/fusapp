401adf97a1bea51cbf77e218351717b2cb1b05b7
Uva <Uva@Uva-PC>
2017-04-23 23:47:59 -0300

    Muestro el detalle de una lista

--- www/js/app/ui/listaUsuario.js
+++ www/js/app/ui/listaUsuario.js
@@ -25,6 +25,64 @@ comunidadfusa.ui.listaUsuario = (function () {
                 $(".fusa-js-lista-canciones-lista").append($("#lista-usuario-tmpl").tmpl(data));
             }
         });
+
+        $(document).off('click', '.fusa-js-descargar-cancion');
+        $(document).on('click', '.fusa-js-descargar-cancion', function (e) {
+            e && e.preventDefault();
+            var $this = $(e.target);
+            if (!$this.is('a')) {
+                $this = $this.closest('a');
+            }
+            var idAudio = $this.data("id");
+            $this.find("i").removeClass("icon-arrow-down");
+            $this.find("i").addClass("icon-clock");
+            comunidadfusa.service.audios.getAudio(idAudio, function (audio) {
+                comunidadfusa.util.descargas.descargarCancion(audio, descargaAudioSuccess, descargaAudioError);
+            });
+        });
+
+        $(document).off('click', '.fusa-js-quitar-descarga-cancion');
+        $(document).on('click', '.fusa-js-quitar-descarga-cancion', function (e) {
+            e && e.preventDefault();
+            var $this = $(e.target);
+            if (!$this.is('a')) {
+                $this = $this.closest('a');
+            }
+            var idAudio = $this.data("id");
+            $this.find("i").removeClass("icon-check");
+            $this.find("i").removeClass("text-success");
+            $this.find("i").addClass("icon-clock");
+            comunidadfusa.service.audios.getAudio(idAudio, function (audio) {
+                comunidadfusa.util.descargas.eliminarCancionDescargada(audio, eliminarAudioSuccess);
+            });
+        });
+    }
+
+    function eliminarAudioSuccess(audioDescargado) {
+        var $cancion = $("a[data-id='" + audioDescargado.id + "']");
+        var $icono = $cancion.find("i.icon-clock");
+        $icono.removeClass("icon-clock");
+        $icono.addClass("icon-arrow-down");
+        $cancion.removeClass("fusa-js-quitar-descarga-cancion");
+    }
+
+    function descargaAudioSuccess(audioDescargado) {
+        var $cancion = $("a[data-id='" + audioDescargado.id + "']");
+        comunidadfusa.service.bandas.incrementarAudiosDescargados(audioDescargado.idBanda);
+        var $icono = $cancion.find("i.icon-clock");
+        $icono.removeClass("icon-clock");
+        $icono.addClass("icon-check");
+        $icono.addClass("text-success");
+        $cancion.removeClass("fusa-js-descargar-cancion");
+        $cancion.addClass("fusa-js-quitar-descarga-cancion");
+    }
+
+    function descargaAudioError(audioDescargado) {
+        var $cancion = $("a[data-id='" + audioDescargado.id + "']");
+        var $icono = $cancion.find("i.icon-clock");
+        $icono.removeClass("icon-clock");
+        $icono.addClass("icon-arrow-down");
+        comunidadfusa.util.analytics.trackEvent("error", "descarga", audioDescargado.id, 1);
     }
 
     return {
