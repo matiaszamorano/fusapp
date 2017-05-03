5a781c42b9b794eca987b6542ba4d783a70cfefb
Uva <Uva@Uva-PC>
2017-05-02 21:41:56 -0300

    refactor de html lista

--- www/js/app/ui/lista.js
+++ www/js/app/ui/lista.js
@@ -2,29 +2,10 @@ comunidadfusa.ui.lista = (function () {
 
     function init() {
         var idLista = comunidadfusa.getUrlParameter("id");
-
-        comunidadfusa.service.listas.getListasUsuario(function (data) {
-            data.forEach(function (lista) {
-                if (lista.id == idLista) {
-                    $(".fusa-js-titulo-lista-usuario").text(lista.nombre);
-                }
-            });
-        });
-
-        comunidadfusa.service.listas.getAudiosListasReproduccion({idListaReproduccion: idLista}, function (data) {
-            if (data.length > 0) {
-                $(".jp-play-me-list").data("id", idLista);
-                data.forEach(function (cancion) {
-                    if (comunidadfusa.service.audios.estaDescargado(cancion.id)) {
-                        cancion.descargado = true;
-                    } else {
-                        cancion.descargado = false;
-                    }
-                });
-                $(".fusa-js-lista-canciones-lista").empty();
-                $(".fusa-js-lista-canciones-lista").append($("#lista-usuario-tmpl").tmpl(data));
-            }
-        });
+        var tipo = comunidadfusa.getUrlParameter("tipo");
+        if (tipo === "usuario") {
+            listaUsuario(idLista);
+        }
 
         $(document).off('click', '.fusa-js-descargar-cancion');
         $(document).on('click', '.fusa-js-descargar-cancion', function (e) {
@@ -55,6 +36,31 @@ comunidadfusa.ui.lista = (function () {
             comunidadfusa.service.audios.getAudio(idAudio, function (audio) {
                 comunidadfusa.util.descargas.eliminarCancionDescargada(audio, eliminarAudioSuccess);
             });
+        });
+    }
+
+    function listaUsuario(idLista) {
+        comunidadfusa.service.listas.getListasUsuario(function (data) {
+            data.forEach(function (lista) {
+                if (lista.id == idLista) {
+                    $(".fusa-js-titulo-lista-usuario").text(lista.nombre);
+                }
+            });
+        });
+
+        comunidadfusa.service.listas.getAudiosListasReproduccion({idListaReproduccion: idLista}, function (data) {
+            if (data.length > 0) {
+                $(".jp-play-me-list").data("id", idLista);
+                data.forEach(function (cancion) {
+                    if (comunidadfusa.service.audios.estaDescargado(cancion.id)) {
+                        cancion.descargado = true;
+                    } else {
+                        cancion.descargado = false;
+                    }
+                });
+                $(".fusa-js-lista-canciones-lista").empty();
+                $(".fusa-js-lista-canciones-lista").append($("#lista-usuario-tmpl").tmpl(data));
+            }
         });
     }
 
