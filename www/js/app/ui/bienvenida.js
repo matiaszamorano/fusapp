1a5b0dcd69b9c85590dd142cbbb2e14945841f24
Uva <Uva@Uva-PC>
2017-05-21 16:46:36 -0300

    Imagenes nuevas para home

--- www/js/app/ui/bienvenida.js
+++ www/js/app/ui/bienvenida.js
@@ -1,10 +1,6 @@
 comunidadfusa.ui.bienvenida = (function () {
 
     function init() {
-        comunidadfusa.service.listas.getListasBandasMasEscuchadas(function (listas) {
-            $("#fusa-js-listas-home").append($("#listaBandaMasEscuchadaTmpl").tmpl(listas));
-
-        });
     }
 
     return {
